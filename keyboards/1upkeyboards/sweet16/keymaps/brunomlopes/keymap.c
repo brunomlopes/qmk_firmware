9eb507c36b861c5c006724c9d2d3ace7889012aa
Bruno Lopes <brunomlopes@gmail.com>
2020-01-06 16:03:14 +0000

    Layout for debug in chrome

--- keyboards/1upkeyboards/sweet16/keymaps/brunomlopes/keymap.c
+++ keyboards/1upkeyboards/sweet16/keymaps/brunomlopes/keymap.c
@@ -2,10 +2,12 @@
 
 enum layers {
  _VISUAL_STUDIO,
+ _CHROME_DEBUGGER,
  _OSX_WINDOW_MANAGER,
  _WINDOW_MANAGER,
 };
 
+/* VS Keycodes */ 
 #define RUN         KC_F5
 #define RUN_WITHOUT_DEBUG  LCTL(KC_F5)
 #define STEP_OVER   KC_F10
@@ -15,13 +17,26 @@ enum layers {
 #define ATTACH_TO   LCA_T(KC_P)
 #define TOGGLE_BREAKPOINT KC_F9
 
+/* Chrome Keycodes */ 
+#define CHROME_REFRESH     KC_F5
+#define CHROME_RUN         KC_F8
+#define CHROME_TOGGLE_BREAKPOINT LCTL(KC_B)
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_VISUAL_STUDIO]=LAYOUT_ortho_4x4(
-      KC_NO                         , STEP_OVER  , STEP_IN           , RUN_TO_LINE,
+      TO(_CHROME_DEBUGGER)          , STEP_OVER  , STEP_IN           , RUN_TO_LINE,
       KC_NO                         , KC_NO      , STEP_OUT          , KC_NO,
       MO(_OSX_WINDOW_MANAGER)       , KC_NO      , KC_NO             , RUN,
       LM(_WINDOW_MANAGER, MOD_LGUI) , ATTACH_TO  , RUN_WITHOUT_DEBUG , TOGGLE_BREAKPOINT
     ),
+
+    [_CHROME_DEBUGGER]=LAYOUT_ortho_4x4(
+      KC_NO                         , STEP_OVER       , STEP_IN   , RUN_TO_LINE,
+      TO(_VISUAL_STUDIO)            , KC_NO           , STEP_OUT  , KC_NO,
+      MO(_OSX_WINDOW_MANAGER)       , KC_NO           , KC_NO     , CHROME_RUN,
+      LM(_WINDOW_MANAGER, MOD_LGUI) , CHROME_REFRESH  , KC_NO     , CHROME_TOGGLE_BREAKPOINT
+    ),
+
     [_OSX_WINDOW_MANAGER]=LAYOUT_ortho_4x4(
       _______, KC_MS_WH_RIGHT , KC_MS_BTN4 , KC_MS_WH_LEFT,
       _______, _______        , _______    , _______,
