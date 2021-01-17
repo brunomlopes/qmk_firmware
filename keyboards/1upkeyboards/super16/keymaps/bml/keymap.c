8331cc8c10c7710970d6ff286da1edac883d6a5f
brunomlopes <brunomlopes@gmail.com>
2021-01-17 20:31:08 +0000

    Lights for layers now work!

--- keyboards/1upkeyboards/super16/keymaps/bml/keymap.c
+++ keyboards/1upkeyboards/super16/keymaps/bml/keymap.c
@@ -95,18 +95,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [_LIGHTS]=LAYOUT_ortho_4x4(
-      _______, KC_NO               , RGB_VAI               , RGB_TOG,
-      _______, KC_NO               , RGB_VAD               , KC_NO,
-      _______, KC_NO               , KC_NO                 , KC_NO,
-      _______, KC_NO               , KC_NO                 , KC_NO
+      OSL(_LAYER_PICKER)           , KC_NO               , RGB_VAI               , RGB_TOG,
+      MO(_MEDIA)                   , KC_NO               , RGB_VAD               , KC_NO,
+      MO(_OSX_WINDOW_MANAGER)      , KC_NO               , KC_NO                 , KC_NO,
+      LM(_WINDOW_MANAGER, MOD_LGUI), KC_NO               , KC_NO                 , KC_NO
     ),
 
 
     [_LAYER_PICKER]=LAYOUT_ortho_4x4(
       _______, DF(_VISUAL_STUDIO) , DF(_CHROME_DEBUGGER)  , KC_NO,
       _______, KC_NO              , KC_NO                 , KC_NO,
-      _______, KC_NO              , KC_NO                 , KC_NO,
-      _______, DF(_MOUSE)         , DF(_LIGHTS)           , KC_NO
+      RESET  , DF(_MOUSE)         , DF(_LIGHTS)           , KC_NO,
+      _______, KC_NO              , KC_NO                 , KC_NO
     ),
 };
 
@@ -141,16 +141,63 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   return true;
 };
 
+// TODO: this should be a sizeof, if I recall
+#define LAYERS_TO_LED_SIZE 6
+const uint16_t PROGMEM layer_to_led[LAYERS_TO_LED_SIZE][2] = {
+    {_VISUAL_STUDIO, 1},
+    {_CHROME_DEBUGGER, 2},
+    {_MEDIA, 4},
+    {_MOUSE, 9},
+    {_LIGHTS, 10},
+    {_LAYER_PICKER, 0},
+};
+
+// BL: do't really like this macro, seems messy, depending on led_layer, etc
+#define CHECK_LAYER_LED(layer, layer_num, led_ix, led_for_layer) if((layer == layer_num) && (led_ix == led_for_layer)) led_layer = true
+
+void bml_set_layer_indicator(layer_state_t state){
+    //int highest_layer = get_highest_layer(default_layer_state);
+    //int highest_layer_d = get_highest_layer(state);
+    // BL: no clue why, but modifiers change the state, but default layer state doesn't
+    // so we check the state for temporary layers, and if there are none, go to the default layer state
+    int highest_layer = get_highest_layer(state);
+    if(highest_layer == 0)
+        highest_layer = get_highest_layer(default_layer_state);
+
+    // uprintf("------------------------------------\n");
+    // uprintf("DEFAULT highest layer: %u . state %u\n", get_highest_layer(default_layer_state), default_layer_state);
+    // uprintf("NORMAL  highest layer: %u . state %u\n", get_highest_layer(state), state);
+
+    for (int led_ix = 0; led_ix < RGBLED_NUM ; led_ix++)
+    {
+        bool led_layer = false;
+        CHECK_LAYER_LED(highest_layer, _LAYER_PICKER, led_ix, 0);
+        CHECK_LAYER_LED(highest_layer, _VISUAL_STUDIO, led_ix, 1);
+        CHECK_LAYER_LED(highest_layer, _CHROME_DEBUGGER, led_ix, 2);
+        CHECK_LAYER_LED(highest_layer, _MEDIA, led_ix, 7);
+        CHECK_LAYER_LED(highest_layer, _MOUSE, led_ix, 9);
+        CHECK_LAYER_LED(highest_layer, _LIGHTS, led_ix, 10);
+        CHECK_LAYER_LED(highest_layer, _WINDOW_MANAGER, led_ix, 15);
+
+        if(led_layer)
+            sethsv(219,65,100, (LED_TYPE *)&led[led_ix]);
+        else
+            sethsv(255,255,0, (LED_TYPE *)&led[led_ix]);
+    }
+    rgblight_set(); // Utility functions do not call rgblight_set() automatically, so they need to be called explicitly.
+}
 
 void keyboard_post_init_user(void) {
 	//Enable the LED layers
-    rgblight_sethsv_noeeprom(HSV_WHITE);
-
 	layer_state_set_user(layer_state);
 }
 
+
 layer_state_t layer_state_set_user(layer_state_t state) {
-  switch (get_highest_layer(state)) {
+    //int highest_default_layer = get_highest_layer(default_layer_state);
+    int highest_layer = get_highest_layer(state);
+    bml_set_layer_indicator(state);
+    switch (highest_layer) {
     case _VISUAL_STUDIO:
         break;
     case _CHROME_DEBUGGER:
@@ -160,14 +207,14 @@ layer_state_t layer_state_set_user(layer_state_t state) {
     case _LIGHTS:
         break;
     case ALT_TAB_LAYER:
-      break;
+        break;
     default:
-      // if we're no longer in ALT_TAB_LAYER but alt_tab is active
-      // leave alt tab mode
-      if(is_alt_tab_active){
+        // if we're no longer in ALT_TAB_LAYER but alt_tab is active
+        // leave alt tab mode
+        if(is_alt_tab_active){
         unregister_code(KC_LALT);
         is_alt_tab_active = false;
-      }
-  }
-  return state;
+        }
+    }
+    return state;
 }
