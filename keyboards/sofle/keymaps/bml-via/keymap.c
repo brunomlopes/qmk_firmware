1eddec5315f1503f4f84bbbec1d9ce6263dce00d
brunomlopes <brunomlopes@gmail.com>
2021-03-07 17:27:55 +0000

    Two more via layers, and a slighly larger tapping term better suited for me.

--- keyboards/sofle/keymaps/bml-via/keymap.c
+++ keyboards/sofle/keymaps/bml-via/keymap.c
@@ -1,19 +1,19 @@
  /* Copyright 2020 Josef Adamcik
   * Modification for VIA support and RGB underglow by Jens Bonk-Wiltfang
-  * 
-  * This program is free software: you can redistribute it and/or modify 
-  * it under the terms of the GNU General Public License as published by 
-  * the Free Software Foundation, either version 2 of the License, or 
-  * (at your option) any later version. 
-  * 
-  * This program is distributed in the hope that it will be useful, 
-  * but WITHOUT ANY WARRANTY; without even the implied warranty of 
-  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
-  * GNU General Public License for more details. 
-  * 
-  * You should have received a copy of the GNU General Public License 
-  * along with this program.  If not, see <http://www.gnu.org/licenses/>. 
-  */ 
+  *
+  * This program is free software: you can redistribute it and/or modify
+  * it under the terms of the GNU General Public License as published by
+  * the Free Software Foundation, either version 2 of the License, or
+  * (at your option) any later version.
+  *
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU General Public License for more details.
+  *
+  * You should have received a copy of the GNU General Public License
+  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+  */
 
 #include QMK_KEYBOARD_H
 #include "oled.c"
@@ -105,9 +105,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [3] = LAYOUT(
   _______, _______ , _______ , _______ , _______ , _______,                           _______,  _______  , _______,  _______ ,  _______ ,_______,
-  _______,  KC_INS,  KC_PSCR,   KC_APP,  XXXXXXX, XXXXXXX,                        KC_PGUP, _______,   KC_UP, _______,_______, KC_BSPC,
-  _______, KC_LALT,  KC_LCTL,  KC_LSFT,  XXXXXXX, KC_CAPS,                       KC_PGDN,  KC_LEFT, KC_DOWN, KC_RGHT,  KC_DEL, KC_BSPC,
-  _______,KC_UNDO, KC_CUT, KC_COPY, KC_PASTE, XXXXXXX,  _______,       _______,  XXXXXXX, _______, XXXXXXX, _______,   XXXXXXX, _______,
+  _______,  _______,  _______,   _______,  _______, _______,                        _______, _______,   _______, _______,_______, _______,
+  _______, _______,  _______,  _______,  _______, _______,                       _______,  _______, _______, _______,  _______, _______,
+  _______,_______, _______, _______, _______, _______,  _______,       _______,  _______, _______, _______, _______,   _______, _______,
+                         _______, _______, _______, _______, _______,       _______, _______, _______, _______, _______
+),
+[4] = LAYOUT(
+  _______, _______ , _______ , _______ , _______ , _______,                           _______,  _______  , _______,  _______ ,  _______ ,_______,
+  _______,  _______,  _______,   _______,  _______, _______,                        _______, _______,   _______, _______,_______, _______,
+  _______, _______,  _______,  _______,  _______, _______,                       _______,  _______, _______, _______,  _______, _______,
+  _______,_______, _______, _______, _______, _______,  _______,       _______,  _______, _______, _______, _______,   _______, _______,
+                         _______, _______, _______, _______, _______,       _______, _______, _______, _______, _______
+),
+[5] = LAYOUT(
+  _______, _______ , _______ , _______ , _______ , _______,                           _______,  _______  , _______,  _______ ,  _______ ,_______,
+  _______,  _______,  _______,   _______,  _______, _______,                        _______, _______,   _______, _______,_______, _______,
+  _______, _______,  _______,  _______,  _______, _______,                       _______,  _______, _______, _______,  _______, _______,
+  _______,_______, _______, _______, _______, _______,  _______,       _______,  _______, _______, _______, _______,   _______, _______,
                          _______, _______, _______, _______, _______,       _______, _______, _______, _______, _______
 )
 };
