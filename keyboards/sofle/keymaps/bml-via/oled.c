757c536b8ccd122660b70134bfae9e8f3ff5e436
brunomlopes <brunomlopes@gmail.com>
2021-04-04 10:59:33 +0100

    The rotary encoders are now multifunction

--- keyboards/sofle/keymaps/bml-via/oled.c
+++ keyboards/sofle/keymaps/bml-via/oled.c
@@ -16,25 +16,36 @@
   */
 
 //Sets up what the OLED screens display.
+#define QMK_KEYBOARD_H "sofle.h"
 
+#include QMK_KEYBOARD_H
+#include "rotary.h"
 
 #ifdef OLED_DRIVER_ENABLE
 
-static void render_logo(void) {
-    static const char PROGMEM qmk_logo[] = {
-        0x80,0x81,0x82,0x83,0x84,0x85,0x86,0x87,0x88,0x89,0x8a,0x8b,0x8c,0x8d,0x8e,0x8f,0x90,0x91,0x92,0x93,0x94,
-        0xa0,0xa1,0xa2,0xa3,0xa4,0xa5,0xa6,0xa7,0xa8,0xa9,0xaa,0xab,0xac,0xad,0xae,0xaf,0xb0,0xb1,0xb2,0xb3,0xb4,
-        0xc0,0xc1,0xc2,0xc3,0xc4,0xc5,0xc6,0xc7,0xc8,0xc9,0xca,0xcb,0xcc,0xcd,0xce,0xcf,0xd0,0xd1,0xd2,0xd3,0xd4,0
-    };
-
-    oled_write_P(qmk_logo, false);
+static void print_rotary_mode(int8_t mode){
+    switch(mode){
+        case ROTARY_MODE_VOLUME:
+            oled_write_P(PSTR("R vol"), false);
+            break;
+        case ROTARY_MODE_VERTICAL_SCROLL:
+            oled_write_ln_P(PSTR("R ^ "), false);
+            break;
+        case ROTARY_MODE_HORIZONTAL_SCROLL:
+            oled_write_ln_P(PSTR("R <>"), false);
+            break;
+        default:
+            oled_write_ln_P(PSTR("R ?"), false);
+    }
 }
 
 static void print_status_narrow(void) {
+    // char status_buffer[7];
+    int highest_layer = get_highest_layer(layer_state);
     // Print current mode
-    oled_write_P(PSTR("\n\n"), false);
+    oled_write_P(PSTR("\n"), false);
 
-    switch (get_highest_layer(layer_state)) {
+    switch (highest_layer) {
         case 0:
             oled_write_ln_P(PSTR("Qwrt"), false);
             break;
@@ -47,8 +58,10 @@ static void print_status_narrow(void) {
     }
     oled_write_P(PSTR("\n\n"), false);
     // Print current layer
-    oled_write_ln_P(PSTR("_____"), false);
-    switch (get_highest_layer(layer_state)) {
+    oled_write_P(PSTR("_ "), false);
+    oled_write_char('0' + highest_layer, false);
+    oled_write_P(PSTR(" _"), false);
+    switch (highest_layer) {
         case 0:
         case 1:
             oled_write_P(PSTR("base "), false);
@@ -68,7 +81,9 @@ static void print_status_narrow(void) {
         default:
             oled_write_ln_P(PSTR("Undef"), false);
     }
-    oled_write_P(PSTR("\n\n\n"), false);
+    print_rotary_mode(left_rotary_current_mode);
+    print_rotary_mode(right_rotary_current_mode);
+    oled_write_P(PSTR("\n\n"), false);
     led_t led_usb_state = host_keyboard_led_state();
     oled_write_ln_P(PSTR("CPSLK"), led_usb_state.caps_lock);
     oled_write_P(PSTR("\n"), false);
@@ -76,18 +91,19 @@ static void print_status_narrow(void) {
 
 }
 
+static void print_status_narrow_right(void) {
+    print_rotary_mode(right_rotary_current_mode);
+}
+
 oled_rotation_t oled_init_user(oled_rotation_t rotation) {
-    if (is_keyboard_master()) {
-        return OLED_ROTATION_270;
-    }
-    return rotation;
+    return OLED_ROTATION_270;
 }
 
 void oled_task_user(void) {
     if (is_keyboard_master()) {
         print_status_narrow();
     } else {
-        render_logo();
+        print_status_narrow_right();
     }
 }
 
---
+++ keyboards/sofle/keymaps/bml-via/rotary.h
@@ -0,0 +1,10 @@
+#pragma once
+
+enum rotary_modes {
+    ROTARY_MODE_VOLUME,
+    ROTARY_MODE_VERTICAL_SCROLL,
+    ROTARY_MODE_HORIZONTAL_SCROLL
+};
+
+extern int right_rotary_current_mode;
+extern int left_rotary_current_mode;
