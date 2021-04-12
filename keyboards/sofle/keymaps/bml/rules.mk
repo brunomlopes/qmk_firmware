b5777d4fb91a3d791086cf8f873716c515f91601
brunomlopes <brunomlopes@gmail.com>
2021-04-12 12:38:29 +0100

    Added underglow to my sofle

--- keyboards/sofle/keymaps/bml/rules.mk
+++ keyboards/sofle/keymaps/bml/rules.mk
@@ -1,4 +1,4 @@
-SRC += oled.c encoder.c
+SRC += oled.c encoder.c underglow.c
 
 OLED_DRIVER_ENABLE = yes
 ENCODER_ENABLE = yes
@@ -6,5 +6,7 @@ CONSOLE_ENABLE = no
 EXTRAKEY_ENABLE = yes
 VIA_ENABLE = no
 LTO_ENABLE = no
-RGBLIGHT_ENABLE = no
+RGBLIGHT_ENABLE = yes
 MOUSEKEY_ENABLE = yes
+
+USER_NAME := brunomlopes
