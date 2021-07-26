fedc7cf19d4cf435b3110d88a8d89c08024c9747
brunomlopes <brunomlopes@gmail.com>
2021-07-26 11:50:40 +0100

    Metalayer, chrome and easier numpad access
    Metalayer is a layer to pick layers, including less common/frequent
    Chrome layer is a duplicate of the super16. Checking if it's useful to not move my hand
    And can now access numpad via lower=>modkeys  for one-handed numbers.

--- keyboards/sofle/keymaps/bml/keymap.c
+++ keyboards/sofle/keymaps/bml/keymap.c
@@ -46,6 +46,25 @@ int right_rotary_current_mode = ROTARY_MODE_VOLUME;
 
 #define KC_BML_TREMA RALT(KC_LBRC)
 
+/* VS Keycodes */
+#define RUN         KC_F5
+#define RUN_WITHOUT_DEBUG  LCTL(KC_F5)
+#define STEP_OVER   KC_F10
+#define STEP_IN     KC_F11
+#define STEP_OUT    LSFT(KC_F11)
+#define RUN_TO_LINE LCTL(KC_F10)
+#define ATTACH_TO   LALT(LCTL(KC_P))
+#define TOGGLE_BREAKPOINT KC_F9
+#define VS_NAVIGATE_TO KC_F12
+
+/* Chrome Keycodes */
+#define CHROME_REFRESH     KC_F5
+#define CHROME_RUN         KC_F8
+#define CHROME_DEVTOOLS    KC_F12
+#define CHROME_TOGGLE_BREAKPOINT LCTL(KC_B)
+#define CHROME_ONOFF_BREAKPOINT LSFT(LCTL(KC_B))
+#define CHROME_PICK_ELEMENT LCTL(LSFT(KC_C))
+
 /*
 - thumb keys have lalt on both sides, to allow for easier access to alt+(left side key) instead of twisting fingers on left.
 - altgr is on symbol layer
@@ -84,11 +103,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [_LOWER] = LAYOUT(
-    KC_DEL  , KC_F1   , KC_F2   , KC_F3         , KC_F4   , KC_F5   ,                          KC_F6       , KC_F7   , KC_F8      , KC_F9     , KC_F10     , KC_F11  ,
-    KC_GRV  , KC_1    , KC_2    , KC_3          , KC_4    , KC_5    ,                          KC_6        , KC_7    , KC_8       , KC_9      , KC_0       , KC_F12  ,
-    KC_TILD , KC_EXLM , KC_AT   , KC_HASH       , KC_DLR  , KC_PERC ,                          KC_CIRC     , KC_AMPR , KC_ASTR    , KC_LPRN   , KC_RPRN    , _______ ,
-    KC_GRV  , KC_UNDS , KC_MINS , A(ALGR(KC_2)) , KC_LCBR , KC_LBRC , _______ ,      _______ , KC_NUBS     , KC_RPRN , S(KC_NUBS) , S(KC_DOT) , S(KC_SLSH) , _______ ,
-                        _______ , _______       , _______ , _______ , KC_SPC  ,      _______ , TT(_NUMPAD) , _______ , _______    , _______
+    KC_DEL  , KC_F1   , KC_F2   , KC_F3          , KC_F4       , KC_F5   ,                                 KC_F6       , KC_F7   , KC_F8      , KC_F9     , KC_F10     , KC_F11  ,
+    KC_GRV  , KC_1    , KC_2    , KC_3           , KC_4        , KC_5    ,                                 KC_6        , KC_7    , KC_8       , KC_9      , KC_0       , KC_F12  ,
+    KC_TILD , KC_EXLM , KC_AT   , KC_HASH        , KC_DLR      , KC_PERC ,                                 KC_CIRC     , KC_AMPR , KC_ASTR    , KC_LPRN   , KC_RPRN    , _______ ,
+    KC_GRV  , KC_UNDS , KC_MINS , A(ALGR(KC_2))  , KC_LCBR     , KC_LBRC , _______ ,      _______        , KC_NUBS     , KC_RPRN , S(KC_NUBS) , S(KC_DOT) , S(KC_SLSH) , _______ ,
+                        _______ , TG(_NUMPADALT) , TG(_NUMPAD) , _______ , KC_SPC  ,      TT(_METALAYER) , TT(_NUMPAD) , _______ , _______    , _______
   ),
 
   [_NAV] = LAYOUT(
@@ -100,21 +119,48 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [_SYMBOL] = LAYOUT(
-    A(KC_F4)          , KC_F13        , KC_F14        , KC_F15        , KC_F16            , XXXXXXX      ,                          XXXXXXX       , KC_MPRV       , KC_MPLY      , KC_MNXT       , XXXXXXX , KC_EQL  ,
-    KC_TAB            , KC_F17        , KC_F18        , A(ALGR(KC_E)) , KC_BML_LAYERA_TAB , KC_ASTR      ,                          A(ALGR(KC_8)) , A(ALGR(KC_9)) , KC_BML_ACUTE , KC_BML_GRAVE  , KC_LBRC , XXXXXXX ,
-    KC_BML_LAYERC_TAB , A(ALGR(KC_2)) , XXXXXXX       , KC_COLN       , S(KC_COMM)        , S(KC_DOT)    ,                          A(ALGR(KC_7)) , A(ALGR(KC_0)) , KC_ASTR      , KC_LPRN       , KC_QUOT , XXXXXXX ,
-    KC_LSFT           , XXXXXXX       , KC_BML_ATILDE , KC_SCLN       , KC_BML_OTILDE     , KC_BML_TREMA , XXXXXXX ,      XXXXXXX , KC_NUBS       , S(KC_NUBS)    , KC_BML_HAT   , KC_BML_TILDE  , KC_SLSH , KC_RSFT ,
-                                        _______       , _______       , _______           , TT(_NUMPAD)  , XXXXXXX ,      XXXXXXX , XXXXXXX       , _______       , _______      , OSM(MOD_RALT)
+    A(KC_F4)          , KC_F13        , KC_F14        , KC_F15        , KC_F16            , XXXXXXX      ,                                 XXXXXXX       , KC_MPRV       , KC_MPLY      , KC_MNXT       , XXXXXXX , KC_EQL  ,
+    KC_TAB            , KC_F17        , KC_F18        , A(ALGR(KC_E)) , KC_BML_LAYERA_TAB , KC_ASTR      ,                                 A(ALGR(KC_8)) , A(ALGR(KC_9)) , KC_BML_ACUTE , KC_BML_GRAVE  , KC_LBRC , XXXXXXX ,
+    KC_BML_LAYERC_TAB , A(ALGR(KC_2)) , XXXXXXX       , KC_COLN       , S(KC_COMM)        , S(KC_DOT)    ,                                 A(ALGR(KC_7)) , A(ALGR(KC_0)) , KC_ASTR      , KC_LPRN       , KC_QUOT , XXXXXXX ,
+    KC_LSFT           , XXXXXXX       , KC_BML_ATILDE , KC_SCLN       , KC_BML_OTILDE     , KC_BML_TREMA , XXXXXXX ,      XXXXXXX        , KC_NUBS       , S(KC_NUBS)    , KC_BML_HAT   , KC_BML_TILDE  , KC_SLSH , KC_RSFT ,
+                                        _______       , _______       , _______           , TT(_NUMPAD)  , XXXXXXX ,      TT(_METALAYER) , XXXXXXX       , _______       , _______      , OSM(MOD_RALT)
   ),
 
   [_NUMPAD] = LAYOUT(
-    KC_BSPC , KC_UP   , KC_P7   , KC_P8   , KC_P9   , KC_PSLS     ,                          TG(_NUMPAD) , ROTARY_MODE_LEFT , ROTARY_MODE_RIGHT , RGB_TOG , KC_CAPS , KC_NLCK ,
-    KC_LEFT , KC_RGHT , KC_P4   , KC_P5   , KC_P6   , KC_PAST     ,                          TG(_GAMING) , XXXXXXX          , XXXXXXX           , XXXXXXX , XXXXXXX , XXXXXXX ,
-    KC_DEL  , KC_DOWN , KC_P1   , KC_P2   , KC_P3   , KC_PMNS     ,                          TG(_HRMOD)  , XXXXXXX          , XXXXXXX           , KC_INS  , KC_DEL  , KC_BSPC ,
-    XXXXXXX , KC_COMM , KC_P0   , KC_PDOT , KC_PENT , KC_PPLS     , RESET   ,      XXXXXXX , XXXXXXX     , XXXXXXX          , XXXXXXX           , XXXXXXX , XXXXXXX , XXXXXXX ,
-                        _______ , _______ , _______ , TT(_NUMPAD) , _______ ,      _______ , _______     , _______          , _______           , _______
+    KC_BSPC , KC_UP   , KC_P7   , KC_P8   , KC_P9       , KC_PSLS     ,                          TG(_NUMPAD)    , ROTARY_MODE_LEFT , ROTARY_MODE_RIGHT , RGB_TOG , KC_CAPS , KC_NLCK ,
+    KC_LEFT , KC_RGHT , KC_P4   , KC_P5   , KC_P6       , KC_PAST     ,                          TG(_GAMING)    , XXXXXXX          , XXXXXXX           , XXXXXXX , XXXXXXX , XXXXXXX ,
+    KC_DEL  , KC_DOWN , KC_P1   , KC_P2   , KC_P3       , KC_PMNS     ,                          TG(_HRMOD)     , XXXXXXX          , XXXXXXX           , KC_INS  , KC_DEL  , KC_BSPC ,
+    XXXXXXX , KC_COMM , KC_P0   , KC_PDOT , KC_PENT     , KC_PPLS     , RESET   ,      XXXXXXX , TG(_NUMPADALT) , TT(_NUMPADALT)   , XXXXXXX           , XXXXXXX , XXXXXXX , XXXXXXX ,
+                        _______ , _______ , TG(_NUMPAD) , TT(_NUMPAD) , _______ ,      _______ , _______        , _______          , _______           , _______
   ),
+
+  [_NUMPADALT] = LAYOUT(
+    KC_BSPC , KC_LEFT , KC_UP   , KC_DOWN        , KC_RGHT , KC_PSLS ,                          XXXXXXX        , XXXXXXX        , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX ,
+    KC_DEL  , KC_P1   , KC_P2   , KC_P3          , KC_P4   , KC_PAST ,                          XXXXXXX        , XXXXXXX        , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX ,
+    KC_P0   , KC_P5   , KC_P6   , KC_P7          , KC_P8   , KC_PMNS ,                          XXXXXXX        , XXXXXXX        , XXXXXXX , KC_INS  , KC_DEL  , KC_BSPC ,
+    KC_COMM , KC_P9   , KC_P0   , KC_PDOT        , KC_PENT , KC_PPLS , XXXXXXX ,      XXXXXXX , TG(_NUMPADALT) , TT(_NUMPADALT) , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX ,
+                        _______ , TG(_NUMPADALT) , _______ , _______ , _______ ,      _______ , _______        , _______        , _______ , _______
+  ),
+
+  [_CHROME] = LAYOUT(
+    XXXXXXX , STEP_OVER           , STEP_IN                 , RUN_TO_LINE              , XXXXXXX , XXXXXXX ,                          XXXXXXX , XXXXXXX     , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX ,
+    XXXXXXX , CHROME_PICK_ELEMENT , STEP_OUT                , KC_NO                    , XXXXXXX , XXXXXXX ,                          XXXXXXX , TG(_CHROME) , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX ,
+    XXXXXXX , CHROME_DEVTOOLS     , KC_NO                   , CHROME_RUN               , XXXXXXX , XXXXXXX ,                          XXXXXXX , XXXXXXX     , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX ,
+    XXXXXXX , CHROME_REFRESH      , CHROME_ONOFF_BREAKPOINT , CHROME_TOGGLE_BREAKPOINT , XXXXXXX , XXXXXXX , XXXXXXX ,      XXXXXXX , XXXXXXX , XXXXXXX     , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX ,
+                                    _______                 , _______                  , _______ , _______ , _______ ,      _______ , _______ , _______     , _______ , _______
+  ),
+
+  [_METALAYER] = LAYOUT(
+    XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX        ,                          TG(_NUMPAD)    , XXXXXXX        , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX ,
+    XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX        ,                          TG(_GAMING)    , TG(_CHROME)    , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX ,
+    XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX        ,                          TG(_HRMOD)     , XXXXXXX        , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX ,
+    XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX        , RESET   ,      XXXXXXX , TG(_NUMPADALT) , TT(_NUMPADALT) , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX ,
+                        _______ , _______ , _______ , TT(_METALAYER) , _______ ,      _______ , _______        , _______        , _______ , _______
+  ),
+
+
 };
+
 
 // Used to extract the basic tapping keycode from a dual-role key.
 // Example: GET_TAP_KC(MT(MOD_RSFT, KC_E)) == KC_E
@@ -279,8 +325,17 @@ void bml_set_layer_indicator(layer_state_t state){
     case _NUMPAD:
         rgblight_sethsv_noeeprom(HSV_BLUE);
         break;
+    case _NUMPADALT:
+        rgblight_sethsv_noeeprom(HSV_GREEN);
+        break;
     case _GAMING:
         rgblight_sethsv_noeeprom(HSV_RED);
+        break;
+    case _CHROME:
+        rgblight_sethsv_noeeprom(HSV_ORANGE);
+        break;
+    case _METALAYER:
+        rgblight_sethsv_noeeprom(HSV_TURQUOISE);
         break;
     case _LOWER:
     case _NAV:
