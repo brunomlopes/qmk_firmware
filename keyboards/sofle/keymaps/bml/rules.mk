e5d37468c2bd38e30b5fd6e136399a1322dfce51
Bruno Lopes <brunomlopes@gmail.com>
2021-11-06 12:07:58 +0000

    Updated to latest master

--- keyboards/sofle/keymaps/bml/rules.mk
+++ keyboards/sofle/keymaps/bml/rules.mk
@@ -1,11 +1,11 @@
 SRC += oled.c encoder.c underglow.c
 
-OLED_DRIVER_ENABLE = yes
+OLED_ENABLE = yes
 ENCODER_ENABLE = yes
 CONSOLE_ENABLE = no
 EXTRAKEY_ENABLE = yes
 VIA_ENABLE = no
-LTO_ENABLE = no
+LTO_ENABLE = yes
 RGBLIGHT_ENABLE = yes
 MOUSEKEY_ENABLE = yes
 
