258776730e652ca7a8d60e223011a5895d096213
Bruno Lopes <brunomlopes@gmail.com>
2021-11-09 12:19:54 +0000

    My layout on the kimiko keyboard.

--- keyboards/keycapsss/kimiko/keymaps/bml/rules.mk
+++ keyboards/keycapsss/kimiko/keymaps/bml/rules.mk
@@ -1,16 +1,18 @@
 SRC += oled.c encoder.c
 
+BOOTLOADER = caterina
+
 OLED_ENABLE = yes
 OLED_DRIVER = SSD1306   # Enables the use of OLED displays
 ENCODER_ENABLE = yes       # ENables the use of one or more encoders
 # do not enable rgb until I fix the led solder jobs
-RGBLIGHT_ENABLE = no      # Enable keyboard RGB underglow
+RGBLIGHT_ENABLE = NO      # Enable keyboard RGB underglow
 
 # LTO: Link Time Optimizations.
 # Reduce compiled size, but will automatically disable the legacy TMK Macros and Functions features.
 # This does not affect QMK Macros and Layers
 LTO_ENABLE = yes
 
-MOUSEKEY_ENABLE = yes
+MOUSEKEY_ENABLE = YES
 
 USER_NAME := brunomlopes
