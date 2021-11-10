7991c39ff34e44aab4bda86c23b489c814b36f0e
Bruno Lopes <brunomlopes@gmail.com>
2021-11-10 09:28:47 +0000

    MT ctrl makes more sense than MT shift
    I kept tripping up on control, but it made sense to pull control from under the palm.
    So, putting control as hold tab feels better.

--- keyboards/sofle/keymaps/bml/keymap.c
+++ keyboards/sofle/keymaps/bml/keymap.c
@@ -75,8 +75,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT(
     REPEAT               , KC_1 , KC_2    , KC_3    , KC_4    , KC_5       ,                                             KC_6        , KC_7    , KC_8    , KC_9    , KC_0    , MO(_HRMOD)    ,
     KC_ESC               , KC_Q , KC_W    , KC_E    , KC_R    , KC_T       ,                                             KC_Y        , KC_U    , KC_I    , KC_O    , KC_P    , KC_RBRC       ,
-    MT(MOD_LSFT, KC_TAB) , KC_A , KC_S    , KC_D    , KC_F    , KC_G       ,                                             KC_H        , KC_J    , KC_K    , KC_L    , KC_ENT  , KC_BSPC       ,
-    KC_LCTL              , KC_Z , KC_X    , KC_C    , KC_V    , KC_B       , KC_MS_BTN1         ,      KC_MUTE         , KC_N        , KC_M    , KC_COMM , KC_DOT  , KC_SLSH , OSM(MOD_RSFT) ,
+    MT(MOD_LCTL, KC_TAB) , KC_A , KC_S    , KC_D    , KC_F    , KC_G       ,                                             KC_H        , KC_J    , KC_K    , KC_L    , KC_ENT  , KC_BSPC       ,
+    KC_LSFT              , KC_Z , KC_X    , KC_C    , KC_V    , KC_B       , KC_MS_BTN1         ,      KC_MUTE         , KC_N        , KC_M    , KC_COMM , KC_DOT  , KC_SLSH , OSM(MOD_RSFT) ,
                                   KC_LCTL , KC_LALT , KC_LGUI , MO(_LOWER) , LT(_SYMBOL,KC_ENT) ,      LT(_NAV,KC_SPC) , MO(_SYMBOL) , KC_RCTL , KC_RGUI , KC_LALT
   ),
 
@@ -91,7 +91,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_GAMING] = LAYOUT(
     KC_GRV              , KC_1 , KC_2    , KC_3    , KC_4    , KC_5   ,                               KC_6        , KC_7           , KC_8    , KC_9    , KC_0    , KC_MINS ,
     KC_ESC              , KC_Q , KC_W    , KC_E    , KC_R    , KC_T   ,                               KC_Y        , KC_U           , KC_I    , KC_O    , KC_P    , KC_RBRC ,
-    MT(MOD_LSFT,KC_TAB) , KC_A , KC_S    , KC_D    , KC_F    , KC_G   ,                               KC_H        , KC_J           , KC_K    , KC_L    , KC_BSLS , KC_BSPC ,
+    MT(MOD_LCTL,KC_TAB) , KC_A , KC_S    , KC_D    , KC_F    , KC_G   ,                               KC_H        , KC_J           , KC_K    , KC_L    , KC_BSLS , KC_BSPC ,
     KC_LSFT             , KC_Z , KC_X    , KC_C    , KC_V    , KC_B   , TG(_GAMING) ,      KC_MUTE  , KC_N        , KC_M           , KC_COMM , KC_DOT  , KC_SLSH , KC_RSFT ,
                                  KC_LCTL , KC_LALT , KC_LCTL , KC_SPC , KC_ENT      ,      MO(_NAV) , MO(_SYMBOL) , TT(_METALAYER) , KC_RGUI , KC_RALT
   ),
