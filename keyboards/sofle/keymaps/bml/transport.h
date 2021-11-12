bf411b1adcb9d8a0c95d33065159fb49d4c84d43
Bruno Lopes <brunomlopes@gmail.com>
2021-11-12 00:25:36 +0000

    Adding split transport to sofle

---
+++ keyboards/sofle/keymaps/bml/transport.h
@@ -0,0 +1,8 @@
+#pragma once
+
+typedef struct _master_to_slave_t {
+    int m2s_rotary_left;
+    int m2s_rotary_right;
+} master_to_slave_t;
+
+extern void user_sync_a_slave_handler(uint8_t in_buflen, const void* in_data, uint8_t out_buflen, void* out_data);
