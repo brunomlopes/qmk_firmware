729bfdd92c4826a46586937ffdfe8fdf8484d1db
Bruno Lopes <brunomlopes@gmail.com>
2021-11-27 01:37:07 +0000

    Re-added rgb to kimiko, now that I've fixed the solder job

--- keyboards/keycapsss/kimiko/keymaps/bml/rules.mk
+++ keyboards/keycapsss/kimiko/keymaps/bml/rules.mk
@@ -6,13 +6,14 @@ OLED_ENABLE = yes
 OLED_DRIVER = SSD1306   # Enables the use of OLED displays
 ENCODER_ENABLE = yes       # ENables the use of one or more encoders
 # do not enable rgb until I fix the led solder jobs
-RGBLIGHT_ENABLE = NO      # Enable keyboard RGB underglow
+EXTRAKEY_ENABLE = yes      
+RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
 
 # LTO: Link Time Optimizations.
 # Reduce compiled size, but will automatically disable the legacy TMK Macros and Functions features.
 # This does not affect QMK Macros and Layers
 LTO_ENABLE = yes
 
-MOUSEKEY_ENABLE = YES
+MOUSEKEY_ENABLE = yes
 
 USER_NAME := brunomlopes
