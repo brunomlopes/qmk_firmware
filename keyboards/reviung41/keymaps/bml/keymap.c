18641f4000debe18e4ed0c5e55481de456ab115b
Bruno Lopes <brunomlopes@gmail.com>
2022-03-12 16:00:30 +0000

    added a toggle for GUI/CTRL/ALT so it's better for osx

--- keyboards/reviung41/keymaps/bml/keymap.c
+++ keyboards/reviung41/keymaps/bml/keymap.c
@@ -69,9 +69,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [_ADJUST] = LAYOUT_reviung41(
-    RESET   , KC_INS  , KC_PSCR , XXXXXXX , RGB_TOG , KC_VOLU ,           KC_APP  , KC_MPRV , KC_MPLY , KC_MNXT , XXXXXXX , XXXXXXX ,
-    _______ , XXXXXXX , KC_EQL  , XXXXXXX , RGB_VAI , KC_VOLD ,           KC_F1   , KC_F2   , KC_F3   , KC_F4   , KC_F5   , KC_F6   ,
-    _______ , XXXXXXX , XXXXXXX , KC_RALT , KC_CAPS , KC_MUTE ,           KC_F7   , KC_F8   , KC_F9   , KC_F10  , KC_F11  , KC_F12  ,
+    RESET   , KC_INS  , KC_PSCR , XXXXXXX , RGB_TOG , KC_VOLU ,           KC_APP  , KC_MPRV , KC_MPLY , KC_MNXT , XXXXXXX , RGB_TOG ,
+    AG_TOGG , XXXXXXX , KC_EQL  , XXXXXXX , RGB_VAI , KC_VOLD ,           KC_F1   , KC_F2   , KC_F3   , KC_F4   , KC_F5   , KC_F6   ,
+    CG_TOGG , XXXXXXX , XXXXXXX , KC_RALT , KC_CAPS , KC_MUTE ,           KC_F7   , KC_F8   , KC_F9   , KC_F10  , KC_F11  , KC_F12  ,
                                             _______ , _______ , _______ , _______ , _______
   ),
 
