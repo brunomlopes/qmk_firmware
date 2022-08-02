08b4858f3b907e9ae627c762ed070a5a623ac74c
Bruno Lopes <brunomlopes@gmail.com>
2022-08-02 18:30:13 +0100

    Ooops, had a typo on the kimiko keymap

--- keyboards/keycapsss/kimiko/keymaps/bml/keymap.c
+++ keyboards/keycapsss/kimiko/keymaps/bml/keymap.c
@@ -82,7 +82,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // |     LSFT     | Z |  X   |  C  |         V         |     B      |      MS_BTN1       |    |      MUTE       |      N      |  M   |  ,  |  .  |  /  | OSM(MOD_RSFT) |
     //                    | LCTL | ALT |        GUI        | MO(_LOWER) | LT(_SYMBOL,KC_ENT) |    | LT(_NAV,KC_SPC) | MO(_SYMBOL) | RCTL | GUI | ALT |
 
-    XXXXXXX              , KC_1 , KC_2    , KC_3    , KC_4              , KC_5       ,                                             KC_6        , KC_7    , KC_8    , KC_9    , KC_0    , MO(_HRMOD)            ,
+    KC_MUTE              , KC_1 , KC_2    , KC_3    , KC_4              , KC_5       ,                                             KC_6        , KC_7    , KC_8    , KC_9    , KC_0    , MO(_HRMOD)            ,
     MT(MOD_LALT, KC_ESC) , KC_Q , KC_W    , KC_E    , KC_R              , KC_T       ,                                             KC_Y        , KC_U    , KC_I    , KC_O    , KC_P    , MT(MOD_LALT, KC_RBRC) ,
     MT(MOD_LCTL, KC_TAB) , KC_A , KC_S    , KC_D    , LT(_LOWERFN,KC_F) , KC_G       ,                                             KC_H        , KC_J    , KC_K    , KC_L    , KC_ENT  , MT(MOD_LCTL, KC_BSPC) ,
     KC_LSFT              , KC_Z , KC_X    , KC_C    , KC_V              , KC_B       , KC_MS_BTN1         ,      KC_MUTE         , KC_N        , KC_M    , KC_COMM , KC_DOT  , KC_SLSH , OSM(MOD_RSFT)         ,
@@ -252,9 +252,9 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
     if(!keep_processing){
         return false;
+    }
 
     switch (keycode) {
-    }
     case ROTARY_MODE_LEFT:
         if (!record->event.pressed) {
             switch(left_rotary_current_mode){
