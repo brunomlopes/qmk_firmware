865ef03871920a652f969fcb59a4b5d1304315c0
Bruno Lopes <brunomlopes@gmail.com>
2022-08-11 20:27:49 +0100

    Layouts on the keys, plus right side J is hold for lower_fn

--- keyboards/keycapsss/kimiko/keymaps/bml/config.h
+++ keyboards/keycapsss/kimiko/keymaps/bml/config.h
@@ -35,8 +35,9 @@
 #endif // TAPPING_TERM
 #define TAPPING_TERM 175
 
-#define TAPPING_TERM_PER_KEY
+#define RGB_MATRIX_MAXIMUM_BRIGHTNESS 30
 
+#define TAPPING_TERM_PER_KEY
 // Read "a guide to homerow mods" for info regarding the next defines for holds,taps, etc
 // https://precondition.github.io/home-row-mods#toc-skipped
 
