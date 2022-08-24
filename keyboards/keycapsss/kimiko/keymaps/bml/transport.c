f1a88b040724b1d98dfefec3997ab8333ba50dee
Bruno Lopes <brunomlopes@gmail.com>
2022-08-24 19:17:33 +0100

    space shift, like space underscore

--- keyboards/keycapsss/kimiko/keymaps/bml/transport.c
+++ keyboards/keycapsss/kimiko/keymaps/bml/transport.c
@@ -15,4 +15,5 @@ void user_sync_a_slave_handler(uint8_t in_buflen, const void* in_data, uint8_t o
     left_rotary_current_mode = m2s->m2s_rotary_left;
     is_caps_word_on_mode = m2s->is_caps_word_on;
     is_bml_spunderscore_active = m2s->is_spunderscore_on;
+    is_bml_spshift_active = m2s->is_spshift_on;
 }
