aace2715eb3f1c942b6cfb6dfdfafdbd0b3dd41d
Bruno Lopes <brunomlopes@gmail.com>
2023-06-19 10:19:28 +0100

    VIA keymap for a one-sided zodiark used as a macropad

---
+++ keyboards/aleblazer/zodiark/keymaps/bml/config.h
@@ -0,0 +1,31 @@
+/*
+Copyright 2021 Spencer Deven <splitlogicdesign@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#pragma once
+
+#ifdef OLED_ENABLE
+  #define OLED_DISPLAY_128X64
+  #define OLED_TIMEOUT 400000
+#endif
+
+#define RGBLIGHT_SLEEP
+#define RGBLIGHT_LAYERS_RETAIN_VAL
+#define DYNAMIC_KEYMAP_LAYER_COUNT 6
+#define RGB_MATRIX_MAXIMUM_BRIGHTNESS 30
+#undef RGBLIGHT_LIMIT_VAL
+#define RGBLIGHT_LIMIT_VAL 60
+
+#undef RGBLIGHT_ANIMATIONS
