2e21769e72dd7f1670f061158c294c26c9a155f2
Bruno Lopes <brunomlopes@gmail.com>
2023-07-02 20:12:00 +0100

    I had forgotten to push this

--- keyboards/cantor/keymaps/bml/keymap.c
+++ keyboards/cantor/keymaps/bml/keymap.c
@@ -15,23 +15,26 @@ enum layers {
   _METALAYER
 };
 
-enum blender_keycode {
-    KC_BML_OTILDE = SAFE_RANGE,  
-    KC_BML_ATILDE, 
-    KC_BML_TILDE,
+enum custom_keycodes {
+    KC_BML_ATILDE,
+    KC_BML_OTILDE,
+    KC_BML_GRAVE,
     KC_BML_ACUTE,
     KC_BML_HAT,
-    KC_BML_TOGGLE_SPSHIFT,
-    KC_BML_TOGGLE_SPUNDERSCORE,
+    KC_BML_TILDE,
+
     KC_BML_FLAYER_FA,
-    KC_BML_GRAVE,
-    KC_BML_TREMA,
+
     KC_BML_LAYERC_TAB,
     KC_BML_LAYERA_TAB,
+
+    KC_BML_TOGGLE_SPUNDERSCORE,
+    KC_BML_TOGGLE_SPSHIFT,
+
+    KC_BML_DOUBLE_CTRL
 };
 
-
-
+#define KC_BML_TREMA RALT(KC_LBRC)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // | LALT| KC_ESC | Q | W |    E     |     R      |  T  |    |  Y  |      U      |  I  | O |  P  |       }       |
@@ -42,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     MT(MOD_LALT, KC_ESC) , KC_Q , KC_W , KC_E     , KC_R              , KC_T   ,      KC_Y   , KC_U        , KC_I    , KC_O   , KC_P    , KC_RBRC               ,
     MT(MOD_LCTL, KC_TAB) , KC_A , KC_S , KC_D     , LT(_LOWERFN,KC_F) , KC_G   ,      KC_H   , KC_J        , KC_K    , KC_L   , KC_ENT  , MT(MOD_LCTL, KC_BSPC) ,
     KC_LSFT              , KC_Z , KC_X , KC_C     , KC_V              , KC_B   ,      KC_N   , KC_M        , KC_COMM , KC_DOT , KC_SLSH , KC_RSFT               ,
-                                         MOD_LGUI , MO(_LOWER)        , KC_ENT ,      KC_SPC , MO(_SYMBOL) , KC_RALT                                            
+                                         KC_LGUI  , MO(_LOWER)        , KC_ENT ,      KC_SPC , MO(_SYMBOL) , KC_RALT                                            
   ),
 
   [_LOWER] = LAYOUT_split_3x6_3(
@@ -69,7 +72,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //    A(KC_F4)          , KC_F13        , KC_F14        , KC_F15        , KC_F16            , KC_F19       ,                                 XXXXXXX       , KC_MPRV       , KC_MPLY      , KC_MNXT       , XXXXXXX                    , KC_EQL                ,
     KC_TAB            , KC_F17        , KC_F18        , A(ALGR(KC_E))         , KC_BML_LAYERA_TAB , KC_F20       ,      A(ALGR(KC_8)) , A(ALGR(KC_9)) , KC_BML_ACUTE , KC_BML_GRAVE , KC_LBRC      , XXXXXXX                    ,
     KC_BML_LAYERC_TAB , A(ALGR(KC_2)) , XXXXXXX       , KC_COLN               , S(KC_COMM)        , S(KC_DOT)    ,      A(ALGR(KC_7)) , A(ALGR(KC_0)) , KC_ASTR      , KC_LPRN      , KC_QUOT      , KC_BSLS                    ,
-    KC_LSFT           , XXXXXXX       , KC_BML_ATILDE , KC_SCLN               , KC_BML_OTILDE     , KC_BML_TREMA ,                      KC_NUBS       , S(KC_NUBS)   , KC_BML_HAT   , KC_BML_TILDE , KC_BML_TOGGLE_SPUNDERSCORE ,
-                                                        KC_BML_TOGGLE_SPSHIFT , _______           , _______      ,      _______       , _______       , _______      , _______      
+    KC_LSFT           , XXXXXXX       , KC_BML_ATILDE , KC_SCLN       , KC_BML_OTILDE     , KC_BML_TREMA ,      KC_NUBS       , S(KC_NUBS)    , KC_BML_HAT   , KC_BML_TILDE , KC_BML_TOGGLE_SPUNDERSCORE , KC_BML_TOGGLE_SPSHIFT ,
+                                                        _______       , _______           , _______      ,      _______       , _______       , _______     
   ),
 };
