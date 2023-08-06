70386c68bf8d2bd536bb6792d962296287353b55
Bruno Lopes <brunomlopes@gmail.com>
2023-08-06 18:38:47 +0100

    left hand numpad

--- keyboards/cantor/keymaps/bml/keymap.c
+++ keyboards/cantor/keymaps/bml/keymap.c
@@ -47,17 +47,17 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT_split_3x6_3(
-    MT(MOD_LALT, KC_ESC) , KC_Q , KC_W , KC_E    , KC_R              , KC_T               ,      KC_Y             , KC_U        , KC_I    , KC_O   , KC_P    , KC_RBRC               ,
-    MT(MOD_LCTL, KC_TAB) , KC_A , KC_S , KC_D    , LT(_LOWERFN,KC_F) , KC_G               ,      KC_H             , KC_J        , KC_K    , KC_L   , KC_ENT  , MT(MOD_LCTL, KC_BSPC) ,
-    KC_LSFT              , KC_Z , KC_X , KC_C    , KC_V              , KC_B               ,      KC_N             , KC_M        , KC_COMM , KC_DOT , KC_SLSH , KC_RSFT               ,
-                                         KC_LGUI , MO(_LOWER)        , LT(_MACRO, KC_ENT) ,      LT(_NAV, KC_SPC) , MO(_SYMBOL) , KC_LALT                                            
+    MT(MOD_LALT, KC_ESC) , KC_Q , KC_W , KC_E    , KC_R               , KC_T                 ,      KC_Y             , KC_U        , KC_I    , KC_O   , KC_P    , KC_RBRC               ,
+    MT(MOD_LCTL, KC_TAB) , KC_A , KC_S , KC_D    , LT(_LOWERFN, KC_F) , LT(_NUMPADALT, KC_G) ,      KC_H             , KC_J        , KC_K    , KC_L   , KC_ENT  , MT(MOD_LCTL, KC_BSPC) ,
+    KC_LSFT              , KC_Z , KC_X , KC_C    , KC_V               , KC_B                 ,      KC_N             , KC_M        , KC_COMM , KC_DOT , KC_SLSH , KC_RSFT               ,
+                                         KC_LGUI , MO(_LOWER)         , LT(_MACRO, KC_ENT)   ,      LT(_NAV, KC_SPC) , MO(_SYMBOL) , KC_LALT                                            
   ),
 
   [_LOWER] = LAYOUT_split_3x6_3(
     KC_BSPC , KC_1    , KC_2    , KC_3          , KC_4    , KC_5    ,      KC_6     , KC_7      , KC_8       , KC_9      , KC_0       , KC_F12  ,
     KC_TILD , KC_EXLM , KC_AT   , KC_HASH       , KC_DLR  , KC_PERC ,      KC_CIRC  , KC_AMPR   , KC_ASTR    , KC_LPRN   , KC_RPRN    , _______ ,
     KC_GRV  , KC_UNDS , KC_MINS , A(ALGR(KC_2)) , KC_LCBR , KC_LBRC ,      KC_NUBS  , KC_RPRN   , S(KC_NUBS) , S(KC_DOT) , S(KC_SLSH) , _______ ,
-                                  KC_LCTL       , _______ , KC_SPC  ,      _______  , MO(_META) , _______                                       
+                                  KC_LCTL       , _______ , KC_SPC  ,      _______  , MO(_META) , _______
   ),
 
   [_LOWER_MAC] = LAYOUT_split_3x6_3(
@@ -74,44 +74,51 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                            XXXXXXX , _______ , _______  ,      KC_BSPC , KC_DEL  , KC_TAB
   ),
 
+  [_NUMPADALT] = LAYOUT_split_3x6_3(
+    XXXXXXX , KC_BSPC , KC_7 , KC_8   , KC_9    , XXXXXXX ,      XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX ,
+    XXXXXXX , KC_TAB  , KC_4 , KC_5   , KC_6    , XXXXXXX ,      XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX ,
+    XXXXXXX , KC_0    , KC_1 , KC_2   , KC_3    , KC_DOT  ,      XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX ,
+                               KC_DOT , KC_COMM , _______ ,      XXXXXXX , XXXXXXX , XXXXXXX                                
+  ),
+
   [_NAV] = LAYOUT_split_3x6_3(
     _______ , KC_INS  , KC_PAUS , KC_APP  , A(KC_F4)        , KC_BTN5 ,      KC_PGUP , KC_HOME    , KC_UP      , KC_END     , C(KC_DEL) , C(KC_BSPC) ,
     _______ , XXXXXXX , XXXXXXX , KC_PSCR , A(ALGR(KC_TAB)) , KC_BTN4 ,      KC_PGDN , KC_LEFT    , KC_DOWN    , KC_RGHT    , KC_DEL    , KC_BSPC    ,
     _______ , C(KC_Z) , C(KC_X) , C(KC_C) , C(KC_V)         , XXXXXXX ,      XXXXXXX , C(KC_LEFT) , C(KC_DOWN) , C(KC_RGHT) , XXXXXXX   , _______    ,
-                                  _______ , _______         , _______ ,      _______ , MO(_MACRO) , _______                                          
+                                  _______ , _______         , _______ ,      _______ , MO(_MACRO) , _______
   ),
 
   [_SYMBOL] = LAYOUT_split_3x6_3(
     // |      TAB       |   F17   |    F18     | ALGR(E) | BML_LAYERA_TAB |    F20    |    | ALGR(8) | ALGR(9) | BML_ACUTE | BML_GRAVE |            {            |      XXXXXXX       |
     // | BML_LAYERC_TAB | ALGR(2) |  XXXXXXX   |    :    |      S(,)      |   S(.)    |    | ALGR(7) | ALGR(0) |   ASTR    |     (     |            '            |         \          |
     // |      LSFT      | XXXXXXX | BML_ATILDE |    ;    |   BML_OTILDE   | BML_TREMA |    |    \    |  S(\)   |  BML_HAT  | BML_TILDE | BML_TOGGLE_SPUNDERSCORE | BML_TOGGLE_SPSHIFT |
-    //                                         | _______ |   MO(_META)    |  _______  |    | _______ | _______ |  _______  |                                                           
+    //                                         | _______ |   MO(_META)    |  _______  |    | _______ | _______ |  _______  |
 
     KC_TAB            , KC_F17        , KC_F18        , A(ALGR(KC_E)) , KC_BML_LAYERA_TAB , KC_F20       ,      A(ALGR(KC_8)) , A(ALGR(KC_9)) , KC_BML_ACUTE , KC_BML_GRAVE , KC_LBRC                    , XXXXXXX               ,
     KC_BML_LAYERC_TAB , A(ALGR(KC_2)) , XXXXXXX       , KC_COLN       , S(KC_COMM)        , S(KC_DOT)    ,      A(ALGR(KC_7)) , A(ALGR(KC_0)) , KC_ASTR      , KC_LPRN      , KC_QUOT                    , KC_BSLS               ,
     KC_LSFT           , XXXXXXX       , KC_BML_ATILDE , KC_SCLN       , KC_BML_OTILDE     , KC_BML_TREMA ,      KC_NUBS       , S(KC_NUBS)    , KC_BML_HAT   , KC_BML_TILDE , KC_BML_TOGGLE_SPUNDERSCORE , KC_BML_TOGGLE_SPSHIFT ,
-                                                        _______       , MO(_META)         , _______      ,      MO(_MACRO)    , _______       , _______                                                                          
+                                                        _______       , MO(_META)         , _______      ,      MO(_MACRO)    , _______       , _______
   ),
 
   [_SYMBOL_MAC] = LAYOUT_split_3x6_3(
     _______ , _______ , _______ , _______ , _______ , _______ ,      _______    , _______    , _______ , _______ , _______ , _______ ,
     _______ , _______ , _______ , _______ , _______ , _______ ,      A(S(KC_8)) , A(S(KC_9)) , _______ , _______ , _______ , _______ ,
     _______ , _______ , _______ , _______ , _______ , _______ ,      _______    , _______    , _______ , _______ , _______ , _______ ,
-                                  _______ , _______ , _______ ,      _______    , _______    , _______                               
+                                  _______ , _______ , _______ ,      _______    , _______    , _______
   ),
 
   [_MACRO] = LAYOUT_split_3x6_3(
     A(KC_F4)           , KC_F13  , KC_F14  , KC_F15            , KC_F16            , KC_F19        ,      KC_AUDIO_VOL_UP   , KC_MPRV , KC_MPLY , KC_MNXT , _______ , _______ ,
     KC_BML_LAYERC_TAB  , KC_F17  , KC_F18  , KC_WH_U           , KC_BML_LAYERA_TAB , KC_F20        ,      KC_AUDIO_VOL_DOWN , _______ , _______ , _______ , _______ , _______ ,
     KC_BML_DOUBLE_CTRL , XXXXXXX , XXXXXXX , KC_WH_D           , XXXXXXX           , KC_AUDIO_MUTE ,      KC_AUDIO_MUTE     , _______ , _______ , _______ , _______ , _______ ,
-                                             KC_AUDIO_VOL_DOWN , KC_AUDIO_VOL_UP   , _______       ,      _______           , _______ , _______                               
+                                             KC_AUDIO_VOL_DOWN , KC_AUDIO_VOL_UP   , _______       ,      _______           , _______ , _______
   ),
 
   [_META] = LAYOUT_split_3x6_3(
     QK_BOOTLOADER  , _______ , _______ , _______ , _______ , _______ ,      _______ , _______ , _______ , _______ , _______ , QK_REBOOT ,
     KC_BML_SHOW_OS , _______ , _______ , _______ , _______ , _______ ,      _______ , _______ , _______ , _______ , _______ , _______   ,
     _______        , _______ , _______ , _______ , _______ , _______ ,      _______ , _______ , _______ , _______ , _______ , _______   ,
-                                         _______ , _______ , _______ ,      _______ , _______ , _______                                 
+                                         _______ , _______ , _______ ,      _______ , _______ , _______
   ),
 };
 
@@ -168,7 +175,7 @@ layer_state_t layer_state_set_user(layer_state_t state){
 // {}
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    mod_state = get_mods(); 
+    mod_state = get_mods();
     os_variant_t detected_os = detected_host_os();
 
     switch (keycode) {
@@ -268,7 +275,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 SEND_STRING("' \n");
                 break;
             }
-        }    
+        }
     case LT(_NAV,KC_SPC):
         if(is_bml_spunderscore_active && record->tap.count && record->event.pressed){
             tap_code16(S(KC_SLSH));
@@ -298,6 +305,7 @@ uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case LT(_NAV,KC_SPC):
         case LT(_LOWERFN,KC_F):
+        case LT(_LOWERFN,KC_G):
         case LT(_LEFTFN,KC_G):
         case LT(_SYMBOL,KC_ENT):
             return TAPPING_TERM + 350;
