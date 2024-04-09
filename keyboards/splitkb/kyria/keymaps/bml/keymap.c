65d3da4a82bbcf14ad6969a298ce53645943a44d
Bruno Lopes <brunomlopes@gmail.com>
2024-04-09 19:14:06 +0100

    Pulled bottom row one col to the middle. dunno if it's better.

--- keyboards/splitkb/kyria/keymaps/bml/keymap.c
+++ keyboards/splitkb/kyria/keymaps/bml/keymap.c
@@ -80,19 +80,17 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_BASE] = LAYOUT(
-    MT(MOD_LALT, KC_ESC) , KC_Q , KC_W , KC_E    , KC_R               , KC_T                 ,                                                                  KC_Y        , KC_U    , KC_I    , KC_O   , KC_P    , KC_RBRC               ,
-    MT(MOD_LCTL, KC_TAB) , KC_A , KC_S , KC_D    , LT(_LOWERFN, KC_F) , LT(_NUMPADALT, KC_G) ,                                                                  KC_H        , KC_J    , KC_K    , KC_L   , KC_ENT  , MT(MOD_LCTL, KC_BSPC) ,
-    KC_LSFT              , KC_Z , KC_X , KC_C    , KC_V               , KC_B                 , _______            , _______ ,      _______ , _______          , KC_N        , KC_M    , KC_COMM , KC_DOT , KC_SLSH , KC_RSFT               ,
-                                         _______ , KC_LGUI            , MO(_LOWER)           , LT(_MACRO, KC_ENT) , _______ ,      _______ , LT(_NAV, KC_SPC) , MO(_SYMBOL) , KC_LALT , _______
+    MT(MOD_LALT, KC_ESC) , KC_Q , KC_W , KC_E    , KC_R               , KC_T                 ,                                                                         KC_Y    , KC_U    , KC_I    , KC_O   , KC_P    , KC_RBRC               ,
+    MT(MOD_LCTL, KC_TAB) , KC_A , KC_S , KC_D    , LT(_LOWERFN, KC_F) , LT(_NUMPADALT, KC_G) ,                                                                         KC_H    , KC_J    , KC_K    , KC_L   , KC_ENT  , MT(MOD_LCTL, KC_BSPC) ,
+    KC_LSFT              , KC_Z , KC_X , KC_C    , KC_V               , KC_B                 , _______    , _______            ,      _______          , _______     , KC_N    , KC_M    , KC_COMM , KC_DOT , KC_SLSH , KC_RSFT               ,
+                                         _______ , _______            , KC_LGUI              , MO(_LOWER) , LT(_MACRO, KC_ENT) ,      LT(_NAV, KC_SPC) , MO(_SYMBOL) , KC_LALT , _______ , _______
   ),
 
-
-
   [_LOWER] = LAYOUT(
-    KC_BSPC , KC_1    , KC_2    , KC_3          , KC_4    , KC_5    ,                                              KC_6      , KC_7    , KC_8       , KC_9      , KC_0       , KC_F12  ,
-    KC_TILD , KC_EXLM , KC_AT   , KC_HASH       , KC_DLR  , KC_PERC ,                                              KC_CIRC   , KC_AMPR , KC_ASTR    , KC_LPRN   , KC_RPRN    , _______ ,
-    KC_GRV  , KC_UNDS , KC_MINS , A(ALGR(KC_2)) , KC_LCBR , KC_LBRC , _______ , _______ ,      _______ , _______ , KC_NUBS   , KC_RPRN , S(KC_NUBS) , S(KC_DOT) , S(KC_SLSH) , _______ ,
-                                  _______       , KC_LCTL , _______ , KC_SPC  , _______ ,      _______ , _______ , MO(_META) , _______ , _______
+    KC_BSPC , KC_1    , KC_2    , KC_3          , KC_4    , KC_5    ,                                                KC_6    , KC_7    , KC_8       , KC_9      , KC_0       , KC_F12  ,
+    KC_TILD , KC_EXLM , KC_AT   , KC_HASH       , KC_DLR  , KC_PERC ,                                                KC_CIRC , KC_AMPR , KC_ASTR    , KC_LPRN   , KC_RPRN    , _______ ,
+    KC_GRV  , KC_UNDS , KC_MINS , A(ALGR(KC_2)) , KC_LCBR , KC_LBRC , _______ , _______ ,      _______ , _______   , KC_NUBS , KC_RPRN , S(KC_NUBS) , S(KC_DOT) , S(KC_SLSH) , _______ ,
+                                  _______       , _______ , KC_LCTL , _______ , KC_SPC  ,      _______ , MO(_META) , _______ , _______ , _______
   ),
 
   [_LOWER_MAC] = LAYOUT(
@@ -103,38 +101,37 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [_LOWERFN] = LAYOUT(
-    KC_DEL  , KC_F1            , KC_F2   , KC_F3   , KC_F4   , KC_F5    ,                                              KC_F6  , KC_F7  , KC_F8   , KC_F9 , KC_F10 , KC_F11 ,
-    KC_BSPC , KC_BML_FLAYER_FA , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX  ,                                              KC_1   , KC_2   , KC_3    , KC_4  , KC_5   , KC_F12 ,
-    KC_TAB  , XXXXXXX          , XXXXXXX , XXXXXXX , XXXXXXX , A(KC_F4) , _______ , _______ ,      _______ , _______ , KC_6   , KC_7   , KC_8    , KC_9  , KC_0   , KC_DOT ,
-                                           _______ , XXXXXXX , _______  , _______ , _______ ,      _______ , KC_BSPC , KC_DEL , KC_TAB , _______
+    KC_DEL  , KC_F1            , KC_F2   , KC_F3   , KC_F4   , KC_F5    ,                                              KC_F6  , KC_F7   , KC_F8   , KC_F9 , KC_F10 , KC_F11 ,
+    KC_BSPC , KC_BML_FLAYER_FA , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX  ,                                              KC_1   , KC_2    , KC_3    , KC_4  , KC_5   , KC_F12 ,
+    KC_TAB  , XXXXXXX          , XXXXXXX , XXXXXXX , XXXXXXX , A(KC_F4) , _______ , _______ ,      _______ , _______ , KC_6   , KC_7    , KC_8    , KC_9  , KC_0   , KC_DOT ,
+                                           _______ , XXXXXXX , _______  , _______ , _______ ,      KC_BSPC , KC_DEL  , KC_TAB , _______ , _______
   ),
 
   [_NUMPADALT] = LAYOUT(
-    XXXXXXX , KC_BSPC , KC_7 , KC_8    , KC_9   , XXXXXXX ,                                              KC_BSPC , KC_7    , KC_8    , KC_9 , XXXXXXX , XXXXXXX ,
-    XXXXXXX , KC_TAB  , KC_4 , KC_5    , KC_6   , XXXXXXX ,                                              KC_TAB  , KC_4    , KC_5    , KC_6 , XXXXXXX , KC_BSPC ,
-    XXXXXXX , KC_0    , KC_1 , KC_2    , KC_3   , KC_DOT  , _______ , _______ ,      _______ , _______ , KC_0    , KC_1    , KC_2    , KC_3 , KC_DOT  , XXXXXXX ,
-                               _______ , KC_DOT , KC_COMM , KC_BSPC , _______ ,      _______ , XXXXXXX , KC_DOT  , KC_COMM , _______
+    XXXXXXX , KC_BSPC , KC_7 , KC_8    , KC_9    , XXXXXXX ,                                              KC_BSPC , KC_7    , KC_8    , KC_9 , XXXXXXX , XXXXXXX ,
+    XXXXXXX , KC_TAB  , KC_4 , KC_5    , KC_6    , XXXXXXX ,                                              KC_TAB  , KC_4    , KC_5    , KC_6 , XXXXXXX , KC_BSPC ,
+    XXXXXXX , KC_0    , KC_1 , KC_2    , KC_3    , KC_DOT  , _______ , _______ ,      _______ , _______ , KC_0    , KC_1    , KC_2    , KC_3 , KC_DOT  , XXXXXXX ,
+                               _______ , _______ , KC_DOT  , KC_COMM , KC_BSPC ,      XXXXXXX , KC_DOT  , KC_COMM , _______ , _______
   ),
 
   [_NAV] = LAYOUT(
-    _______ , KC_INS  , KC_PAUS , KC_APP  , A(KC_F4)        , KC_BTN5 ,                                              KC_PGUP    , KC_HOME    , KC_UP      , KC_END     , C(KC_DEL) , C(KC_BSPC) ,
-    _______ , XXXXXXX , XXXXXXX , KC_PSCR , A(ALGR(KC_TAB)) , KC_BTN4 ,                                              KC_PGDN    , KC_LEFT    , KC_DOWN    , KC_RGHT    , KC_DEL    , KC_BSPC    ,
-    _______ , C(KC_Z) , C(KC_X) , C(KC_C) , C(KC_V)         , XXXXXXX , _______ , _______ ,      _______ , _______ , XXXXXXX    , C(KC_LEFT) , C(KC_DOWN) , C(KC_RGHT) , XXXXXXX   , _______    ,
-                                  _______ , _______         , _______ , _______ , _______ ,      _______ , _______ , MO(_MACRO) , _______    , _______
+    _______ , KC_INS  , KC_PAUS , KC_APP  , A(KC_F4)        , KC_BTN5 ,                                                 KC_PGUP , KC_HOME    , KC_UP      , KC_END     , C(KC_DEL) , C(KC_BSPC) ,
+    _______ , XXXXXXX , XXXXXXX , KC_PSCR , A(ALGR(KC_TAB)) , KC_BTN4 ,                                                 KC_PGDN , KC_LEFT    , KC_DOWN    , KC_RGHT    , KC_DEL    , KC_BSPC    ,
+    _______ , C(KC_Z) , C(KC_X) , C(KC_C) , C(KC_V)         , XXXXXXX , _______ , _______ ,      _______ , _______    , XXXXXXX , C(KC_LEFT) , C(KC_DOWN) , C(KC_RGHT) , XXXXXXX   , _______    ,
+                                  _______ , _______         , _______ , _______ , _______ ,      _______ , MO(_MACRO) , _______ , _______    , _______
   ),
 
   [_SYMBOL] = LAYOUT(
-    // |      TAB       |   F17   |    F18     | ALGR(E) | BML_LAYERA_TAB |    F20                        |                           | ALGR(8) | ALGR(9) | BML_ACUTE | BML_GRAVE |            {            |      XXXXXXX       |
-    // | BML_LAYERC_TAB | ALGR(2) |  XXXXXXX   |    :    |      S(,)      |   S(.)                        |                           | ALGR(7) | ALGR(0) |   ASTR    |     (     |            '            |         \          |
-    // |      LSFT      | XXXXXXX | BML_ATILDE |    ;    |   BML_OTILDE   | BML_TREMA | _______ | _______ |    | _______ |  _______   |    \    |  S(\)   |  BML_HAT  | BML_TILDE | BML_TOGGLE_SPUNDERSCORE | BML_TOGGLE_SPSHIFT |
-    //                                         | _______ |    _______     | MO(_META) | _______ | _______ |    | _______ | MO(_MACRO) | _______ | _______ |  _______  |
+    // |      TAB       |   F17   |    F18     | ALGR(E) | BML_LAYERA_TAB |    F20                          |                           | ALGR(8) | ALGR(9) | BML_ACUTE | BML_GRAVE |            {            |      XXXXXXX       |
+    // | BML_LAYERC_TAB | ALGR(2) |  XXXXXXX   |    :    |      S(,)      |   S(.)                          |                           | ALGR(7) | ALGR(0) |   ASTR    |     (     |            '            |         \          |
+    // |      LSFT      | XXXXXXX | BML_ATILDE |    ;    |   BML_OTILDE   | BML_TREMA |  _______  | _______ |    |  _______   | _______ |    \    |  S(\)   |  BML_HAT  | BML_TILDE | BML_TOGGLE_SPUNDERSCORE | BML_TOGGLE_SPSHIFT |
+    //                                         | _______ |    _______     |  _______  | MO(_META) | _______ |    | MO(_MACRO) | _______ | _______ | _______ |  _______  |
 
-    KC_TAB            , KC_F17        , KC_F18        , A(ALGR(KC_E)) , KC_BML_LAYERA_TAB , KC_F20       ,                                                 A(ALGR(KC_8)) , A(ALGR(KC_9)) , KC_BML_ACUTE , KC_BML_GRAVE , KC_LBRC                    , XXXXXXX               ,
-    KC_BML_LAYERC_TAB , A(ALGR(KC_2)) , XXXXXXX       , KC_COLN       , S(KC_COMM)        , S(KC_DOT)    ,                                                 A(ALGR(KC_7)) , A(ALGR(KC_0)) , KC_ASTR      , KC_LPRN      , KC_QUOT                    , KC_BSLS               ,
-    KC_LSFT           , XXXXXXX       , KC_BML_ATILDE , KC_SCLN       , KC_BML_OTILDE     , KC_BML_TREMA , _______ , _______ ,      _______ , _______    , KC_NUBS       , S(KC_NUBS)    , KC_BML_HAT   , KC_BML_TILDE , KC_BML_TOGGLE_SPUNDERSCORE , KC_BML_TOGGLE_SPSHIFT ,
-                                                        _______       , _______           , MO(_META)    , _______ , _______ ,      _______ , MO(_MACRO) , _______       , _______       , _______
+    KC_TAB            , KC_F17        , KC_F18        , A(ALGR(KC_E)) , KC_BML_LAYERA_TAB , KC_F20       ,                                                   A(ALGR(KC_8)) , A(ALGR(KC_9)) , KC_BML_ACUTE , KC_BML_GRAVE , KC_LBRC                    , XXXXXXX               ,
+    KC_BML_LAYERC_TAB , A(ALGR(KC_2)) , XXXXXXX       , KC_COLN       , S(KC_COMM)        , S(KC_DOT)    ,                                                   A(ALGR(KC_7)) , A(ALGR(KC_0)) , KC_ASTR      , KC_LPRN      , KC_QUOT                    , KC_BSLS               ,
+    KC_LSFT           , XXXXXXX       , KC_BML_ATILDE , KC_SCLN       , KC_BML_OTILDE     , KC_BML_TREMA , _______   , _______ ,      _______    , _______ , KC_NUBS       , S(KC_NUBS)    , KC_BML_HAT   , KC_BML_TILDE , KC_BML_TOGGLE_SPUNDERSCORE , KC_BML_TOGGLE_SPSHIFT ,
+                                                        _______       , _______           , _______      , MO(_META) , _______ ,      MO(_MACRO) , _______ , _______       , _______       , _______
   ),
-
 
   [_SYMBOL_MAC] = LAYOUT(
     _______ , _______ , _______ , _______ , _______ , _______ ,                                              _______    , _______    , _______ , _______ , _______ , _______ ,
