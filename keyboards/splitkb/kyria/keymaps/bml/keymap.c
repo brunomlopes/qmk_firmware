4e4d4849626711e3d6c83fa3dcd01c464e354763
Bruno Lopes <brunomlopes@gmail.com>
2024-08-13 16:31:40 +0100

    Mouse layer, plus right-side numpad

--- keyboards/splitkb/kyria/keymaps/bml/keymap.c
+++ keyboards/splitkb/kyria/keymaps/bml/keymap.c
@@ -78,17 +78,18 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_BASE] = LAYOUT(
-    MT(MOD_LALT, KC_ESC) , KC_Q , KC_W , KC_E    , KC_R               , KC_T                 ,                                                                         LT(_MOUSE, KC_Y) , KC_U    , KC_I    , KC_O   , KC_P    , KC_RBRC               ,
-    MT(MOD_LCTL, KC_TAB) , KC_A , KC_S , KC_D    , LT(_LOWERFN, KC_F) , LT(_NUMPADALT, KC_G) ,                                                                         KC_H             , KC_J    , KC_K    , KC_L   , KC_ENT  , MT(MOD_LCTL, KC_BSPC) ,
-    KC_LSFT              , KC_Z , KC_X , KC_C    , KC_V               , LT(_NAVALT, KC_B)    , MO(_META)  , KC_MS_BTN1         ,      KC_MS_BTN2       , MO(_MACRO)  , KC_N             , KC_M    , KC_COMM , KC_DOT , KC_SLSH , KC_RSFT               ,
+    MT(MOD_LALT, KC_ESC) , KC_Q , KC_W , KC_E    , KC_R               , LT(_MOUSE, KC_T)     ,                                                                         LT(_MOUSE, KC_Y) , KC_U    , KC_I    , KC_O   , LT(_NUMPADALT, KC_P) , KC_RBRC               ,
+    MT(MOD_LCTL, KC_TAB) , KC_A , KC_S , KC_D    , LT(_LOWERFN, KC_F) , LT(_NUMPADALT, KC_G) ,                                                                         KC_H             , KC_J    , KC_K    , KC_L   , KC_ENT               , MT(MOD_LCTL, KC_BSPC) ,
+    KC_LSFT              , KC_Z , KC_X , KC_C    , KC_V               , LT(_NAVALT, KC_B)    , MO(_META)  , KC_MS_BTN1         ,      KC_MS_BTN2       , MO(_MACRO)  , KC_N             , KC_M    , KC_COMM , KC_DOT , KC_SLSH              , KC_RSFT               ,
                                          KC_LALT , KC_LCTL            , KC_LGUI              , MO(_LOWER) , LT(_MACRO, KC_ENT) ,      LT(_NAV, KC_SPC) , MO(_SYMBOL) , KC_LALT          , KC_RCTL , KC_RGUI
   ),
 
+
   [_LOWER] = LAYOUT(
-    KC_BSPC , KC_1    , KC_2    , KC_3          , KC_4    , KC_5    ,                                                    KC_6    , KC_7    , KC_8       , KC_9      , KC_0       , KC_F12  ,
-    KC_TILD , KC_EXLM , KC_AT   , KC_HASH       , KC_DLR  , KC_PERC ,                                                    KC_CIRC , KC_AMPR , KC_ASTR    , KC_LPRN   , KC_RPRN    , _______ ,
-    KC_GRV  , KC_UNDS , KC_MINS , A(ALGR(KC_2)) , KC_LCBR , KC_LBRC , MO(_NAVALT) , _______ ,      _______ , _______   , KC_NUBS , KC_RPRN , S(KC_NUBS) , S(KC_DOT) , S(KC_SLSH) , _______ ,
-                                  _______       , _______ , KC_LCTL , _______     , KC_SPC  ,      _______ , MO(_META) , _______ , _______ , _______
+    KC_BSPC , KC_1    , KC_2    , KC_3          , KC_4    , KC_5    ,                                                  KC_6    , KC_7    , KC_8       , KC_9      , KC_0       , KC_F12  ,
+    KC_TILD , KC_EXLM , KC_AT   , KC_HASH       , KC_DLR  , KC_PERC ,                                                  KC_CIRC , KC_AMPR , KC_ASTR    , KC_LPRN   , KC_RPRN    , _______ ,
+    KC_GRV  , KC_UNDS , KC_MINS , A(ALGR(KC_2)) , KC_LCBR , KC_LBRC , MO(_NAVALT) , _______ ,      _______ , _______ , KC_NUBS , KC_RPRN , S(KC_NUBS) , S(KC_DOT) , S(KC_SLSH) , _______ ,
+                                  _______       , _______ , KC_LCTL , _______     , KC_SPC  ,      _______ , XXXXXXX , _______ , _______ , _______
   ),
 
   [_LOWER_MAC] = LAYOUT(
@@ -108,7 +109,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_NUMPADALT] = LAYOUT(
     XXXXXXX , KC_BSPC , KC_7 , KC_8    , KC_9    , XXXXXXX ,                                              KC_BSPC , KC_7    , KC_8    , KC_9 , XXXXXXX , XXXXXXX ,
     XXXXXXX , KC_TAB  , KC_4 , KC_5    , KC_6    , XXXXXXX ,                                              KC_TAB  , KC_4    , KC_5    , KC_6 , XXXXXXX , KC_BSPC ,
-    XXXXXXX , KC_0    , KC_1 , KC_2    , KC_3    , KC_DOT  , _______ , _______ ,      _______ , _______ , KC_0    , KC_1    , KC_2    , KC_3 , KC_DOT  , XXXXXXX ,
+    XXXXXXX , KC_0    , KC_1 , KC_2    , KC_3    , KC_DOT  , _______ , KC_DEL  ,      _______ , _______ , KC_0    , KC_1    , KC_2    , KC_3 , KC_DOT  , XXXXXXX ,
                                _______ , _______ , KC_DOT  , KC_COMM , KC_BSPC ,      KC_SPC  , KC_DOT  , KC_COMM , _______ , _______
   ),
 
@@ -129,7 +130,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_MOUSE] = LAYOUT(
     _______ , _______ , _______ , _______ , _______ , _______ ,                                                    _______    , KC_MS_BTN4    , KC_MS_BTN5 , _______ , _______ , _______ ,
     _______ , _______ , _______ , _______ , _______ , _______ ,                                                    _______    , _______    , _______ , _______ , _______ , _______ ,
-    _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ ,      KC_MS_BTN3 , KC_MS_BTN3 , _______    , _______    , _______ , _______ , _______ , _______ ,
+    _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ ,      KC_MS_BTN1 , KC_MS_BTN3 , _______    , _______    , _______ , _______ , _______ , _______ ,
                                   _______ , _______ , _______ , _______ , _______ ,      KC_MS_BTN1 , KC_MS_BTN2 , _______    , _______    , _______
   ),
 
@@ -142,7 +143,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB            , KC_F17        , KC_F18        , A(ALGR(KC_E)) , KC_BML_LAYERA_TAB , KC_F20       ,                                                   A(ALGR(KC_8)) , A(ALGR(KC_9)) , KC_BML_ACUTE , KC_BML_GRAVE , KC_LBRC                    , XXXXXXX               ,
     KC_BML_LAYERC_TAB , A(ALGR(KC_2)) , XXXXXXX       , KC_COLN       , S(KC_COMM)        , S(KC_DOT)    ,                                                   A(ALGR(KC_7)) , A(ALGR(KC_0)) , KC_ASTR      , KC_LPRN      , KC_QUOT                    , KC_BSLS               ,
     KC_LSFT           , XXXXXXX       , KC_BML_ATILDE , KC_SCLN       , KC_BML_OTILDE     , KC_BML_TREMA , _______   , _______ ,      _______    , _______ , KC_NUBS       , S(KC_NUBS)    , KC_BML_HAT   , KC_BML_TILDE , KC_BML_TOGGLE_SPUNDERSCORE , KC_BML_TOGGLE_SPSHIFT ,
-                                                        _______       , _______           , _______      , MO(_META) , _______ ,      MO(_MACRO) , _______ , _______       , _______       , _______
+                                                        _______       , _______           , _______      , XXXXXXX   , _______ ,      MO(_MACRO) , _______ , _______       , _______       , _______
   ),
 
   [_SYMBOL_MAC] = LAYOUT(
@@ -160,11 +161,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [_META] = LAYOUT(
-    QK_BOOTLOADER  , _______ , _______ , KC_SYSTEM_SLEEP , _______ , LGUI(KC_L) ,                                              _______ , _______ , _______ , _______ , _______  , QK_REBOOT ,
-    KC_BML_SHOW_OS , _______ , _______ , KC_SYSTEM_WAKE  , _______ , _______    ,                                              RGB_TOG , RGB_SAI , RGB_HUI , RGB_VAI , RGB_MOD  , _______   ,
-    _______        , _______ , _______ , _______         , _______ , _______    , _______ , _______ ,      _______ , _______ , _______ , RGB_SAD , RGB_HUD , RGB_VAD , RGB_RMOD , _______   ,
-                                         _______         , _______ , _______    , _______ , _______ ,      _______ , _______ , _______ , _______ , _______
+    QK_BOOTLOADER  , _______ , _______ , KC_SYSTEM_SLEEP , _______      , LGUI(KC_L)  ,                                              _______ , _______ , _______ , _______ , _______  , QK_REBOOT ,
+    KC_BML_SHOW_OS , _______ , _______ , KC_SYSTEM_WAKE  , _______      , _______     ,                                              RGB_TOG , RGB_SAI , RGB_HUI , RGB_VAI , RGB_MOD  , _______   ,
+    _______        , _______ , _______ , _______         , KC_CAPS_LOCK , KC_NUM_LOCK , _______ , _______ ,      _______ , _______ , _______ , RGB_SAD , RGB_HUD , RGB_VAD , RGB_RMOD , _______   ,
+                                         _______         , _______      , _______     , _______ , _______ ,      _______ , _______ , _______ , _______ , _______
   ),
+
 
 };
 
